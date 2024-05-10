29b17a1cf316d3b9d7e4f002a8b6e26689fad135
Trọng Nguyễn <94695234+trongnguyen2801@users.noreply.github.com>
2024-05-10 18:35:52 +0700

    feat: player slide when attack

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -15,6 +15,15 @@ public class Character : MonoBehaviour
 
     private float _gravity = -0.9f;
     public bool isPlayer = true;
+    private float _attackAnimationDuration;
+    
+    //PlayerSlide
+    public float attackStartTime;
+    public float attackSlideDuraton = 0.4f;
+    public float attackSlideSpeed = 0.1f;
+    
+    //Combo Melee
+    public float attackCoolDown = 1f;
     
     public enum CharacterState
     {
@@ -31,6 +40,11 @@ public class Character : MonoBehaviour
 
     private void CalculateMovementVelocity()
     {
+        if (_characterInput.mouseButtonDown && _cc.isGrounded)
+        {
+            SwitchStateTo(CharacterState.Attacking);
+            return;
+        }
         _movementVelocity.Set(_characterInput.horizontalInput, 0f, _characterInput.verticalInput);
         _movementVelocity.Normalize();
         _movementVelocity = Quaternion.Euler(0, -45f, 0) * _movementVelocity;
@@ -45,7 +59,34 @@ public class Character : MonoBehaviour
 
     private void FixedUpdate()
     {
-        CalculateMovementVelocity();
+        switch (CurrentState)
+        {
+            case CharacterState.Normal:
+                CalculateMovementVelocity();
+                break;
+            case CharacterState.Attacking:
+                if (Time.deltaTime < attackSlideDuraton + attackStartTime)
+                {
+                    float timePassed = Time.time - attackStartTime;
+                    float lerpTime = timePassed / attackSlideDuraton;
+                    _movementVelocity = Vector3.Lerp(transform.forward * attackSlideSpeed, Vector3.zero, lerpTime);
+                }
+                if (_characterInput.mouseButtonDown && _cc.isGrounded)
+                {
+                    _attackAnimationDuration = _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+                    if (_animator.GetCurrentAnimatorClipInfo(0)[0].clip.name != "Male Attack 3" && _attackAnimationDuration > 0.7f)
+                    {
+                        _characterInput.mouseButtonDown = false;
+                        SwitchStateTo(CharacterState.Attacking);
+                        CalculateMovementVelocity();
+                    }
+                }
+                break;
+            case CharacterState.Slide:
+                break;
+            case CharacterState.Dead:
+                break;
+        }
         
         if (isPlayer)
         {
@@ -92,6 +133,8 @@ public class Character : MonoBehaviour
             case CharacterState.Normal:
                 break;
             case CharacterState.Attacking:
+                _animator.SetTrigger("Attack");
+                attackStartTime = Time.time;
                 break;
             case CharacterState.Sprint:
                 break;
@@ -102,5 +145,12 @@ public class Character : MonoBehaviour
             case CharacterState.Dead:
                 break;
         }
+
+        CurrentState = newState;
+    }
+    
+    public void AttackAnimationEnds()
+    {
+        SwitchStateTo(CharacterState.Normal);
     }
 }
