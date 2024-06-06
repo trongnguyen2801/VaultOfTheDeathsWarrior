d7c633ea782704736938cd3bc281702c1c28708f
Trọng Nguyễn <94695234+trongnguyen2801@users.noreply.github.com>
2024-06-06 22:30:42 +0700

    update: equip hero in start scene

--- Assets/Scripts/StartSceneManager.cs
+++ Assets/Scripts/StartSceneManager.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DissolveExample;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartSceneManager : MonoBehaviour
 {
@@ -15,26 +17,44 @@ public class StartSceneManager : MonoBehaviour
     public GameObject lightMale;
     public GameObject lightFemale;
     
-    public GameObject textNameMale;
-    public GameObject textNamefemale;
+    public TextMeshProUGUI textNameMale;
+    public TextMeshProUGUI textNameFemale;
+    
+    List<Material> materials = new List<Material>();
 
     private void Start()
     {
-        
+        var renders = GetComponentsInChildren<Renderer>();
+        for (int i = 0; i < renders.Length; i++)
+        {
+            materials.AddRange(renders[i].materials);
+        }
+
+        SetValue(0);
+    }
+    
+    public void SetValue(float value)
+    {
+        for (int i = 0; i < materials.Count; i++)
+        {
+            materials[i].SetFloat("_Dissolve", value);
+        }
     }
 
     public void SelectFemalePlayer()
     {
         lightFemale.SetActive(true);
+        textNameFemale.gameObject.SetActive(true);
         animFemale.SetTrigger("Attack");
         lightMale.SetActive(false);
-        
+        textNameMale.gameObject.SetActive(false);
     }
     public void SelectMalePlayer()
     {
         lightFemale.SetActive(false);
+        textNameFemale.gameObject.SetActive(false);
         lightMale.SetActive(true);
+        textNameMale.gameObject.SetActive(true);
         animMale.SetTrigger("Attack");
     }
-    
 }
